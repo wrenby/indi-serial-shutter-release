0a5bf3961f8dacaa1d27e2c5b863280aaf04f44f
Wren Baxter <git@wren.dev>
2022-05-09 18:43:40 -0400

    changed capture format name so it can be detected by INDI

--- driver.cpp
+++ driver.cpp
@@ -99,7 +99,7 @@ void SerialShutterReleaseCCD::ISGetProperties(const char * dev) {
 
 bool SerialShutterReleaseCCD::Connect() {
     // CCD drivers have to report some kind of capture format
-    CaptureFormat format = {"FORMAT_NONE", "None", 8, false};
+    CaptureFormat format = {"FORMAT_1", "None", 8, false};
     addCaptureFormat(format);
 
     if (PortTP.tp[0].text && strlen(PortTP.tp[0].text) > 0) {
